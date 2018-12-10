3e28aabc1c4a7509064e38d84ae5d045b8680612
Geraldine13 <geral1312@hotmail.com>
2018-12-10 08:16:13 -0500

    Updates

---
+++ precache-manifest.95b891fde160094690893883e0169adc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f9cd906025de2060e2dddcbc278d54a",
+    "url": "/memoryCards/static/media/reina-corazon.4f9cd906.png"
+  },
+  {
+    "revision": "6074ea6357d6361f21f2",
+    "url": "/memoryCards/static/css/main.4e5dfa98.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/memoryCards/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "aca03d3d9712c696ede0",
+    "url": "/memoryCards/static/js/1.aca03d3d.chunk.js"
+  },
+  {
+    "revision": "12bbe9fed2379c457c21",
+    "url": "/memoryCards/static/js/runtime~main.12bbe9fe.js"
+  },
+  {
+    "revision": "238b9c53c0e4d9bdad0c8d8c3e2bb129",
+    "url": "/memoryCards/static/media/az-trebol.238b9c53.png"
+  },
+  {
+    "revision": "c83f182be7cbb724fd037bf5c50d26a4",
+    "url": "/memoryCards/static/media/nueve-corazon.c83f182b.png"
+  },
+  {
+    "revision": "6074ea6357d6361f21f2",
+    "url": "/memoryCards/static/js/main.6074ea63.chunk.js"
+  },
+  {
+    "revision": "28465b01b73158e911e7941ae247480f",
+    "url": "/memoryCards/static/media/dos-espada.28465b01.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/memoryCards/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/memoryCards/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/memoryCards/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/memoryCards/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "aca03d3d9712c696ede0",
+    "url": "/memoryCards/static/css/1.fb57653d.chunk.css"
+  },
+  {
+    "revision": "8b26d75b6444484c7f2369a5d4c97f7f",
+    "url": "/memoryCards/index.html"
+  }
+];
